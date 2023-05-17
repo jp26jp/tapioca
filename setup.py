8f0d193a00b9254bc2c80af21a0e3ccf8a026473
John Perry <john@j-26.com>
2023-05-17 18:34:21 -0500

    Changed project name back

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ if sys.argv[-1] == 'publish':
 
 
 setup(
-    name='tapioca-wrapper',
+    name='tapioca',
     version=get_version(package),
     description='Python API client generator',
     long_description=description,
