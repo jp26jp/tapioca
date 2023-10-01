57fbeaaf21d602b91d30162bc3d3c753d9025227
John Perry <john@j-26.com>
2023-10-01 13:40:45 -0600

    Fixed division by zero error

--- tapioca/__init__.py
+++ tapioca/__init__.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 __author__ = 'Filipe Ximenes'
 __email__ = 'filipeximenes@gmail.com'
-__version__ = '2.3.3'
+__version__ = '2.3.4'
 
 
 from .adapters import (
